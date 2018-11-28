b0a30f3223e2ceae95b13f7c5b94f3bee5cf4de5
Nikolai Maltsev <nsmalcev@gmail.com>
2018-11-28 14:04:23 +0100

    add profile view

--- src/app/ui/legend-profile-card/legend-profile-card.component.ts
+++ src/app/ui/legend-profile-card/legend-profile-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SolidProfile } from '../../models/solid-profile.model';
 
 @Component({
   selector: 'legend-profile-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./legend-profile-card.component.css']
 })
 export class LegendProfileCardComponent implements OnInit {
-
+  @Input() userProfile: SolidProfile; 
   constructor() { }
 
   ngOnInit() {
