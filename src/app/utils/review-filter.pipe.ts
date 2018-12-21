e59ea8f62315ea6086efb133ac0b85538c673e0d
Cristian Grigoriu <grrigoriu@gmail.com>
2018-12-21 14:02:10 +0100

    Search by GlobalId (osm_id)

--- src/app/utils/review-filter.pipe.ts
+++ src/app/utils/review-filter.pipe.ts
@@ -10,9 +10,22 @@ export class ReviewFilterPipe implements PipeTransform {
     let filtered = reviews;
     if (queryIds.length > 0) {
       filtered = reviews.filter(review => {
-        if (review.thing && review.thing.globalIdentifier) {
-          return queryIds.indexOf(review.thing.globalIdentifier) !== -1;
+        // TODO optimize and sort by relevance
+        let isChosen = false;
+        if (review.thing) {
+          if (review.thing.globalIdentifier) {
+            isChosen = isChosen || queryIds.indexOf(review.thing.globalIdentifier) !== -1;
+          }
+          if (review.thing.address) {
+            if (review.thing.address.countryGlobalId) {
+              isChosen = isChosen || queryIds.indexOf(review.thing.address.countryGlobalId) !== -1;
+            }
+            if (review.thing.address.localityGlobalId) {
+              isChosen = isChosen || queryIds.indexOf(review.thing.address.localityGlobalId) !== -1;
+            }
+          }
         }
+        return isChosen;
       });
     }
     return filtered;
